d7db748229660a6d734cbef2a589e9178a1db920
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-07-20 14:54:40 +0200

    done

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/11 16:55:21 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/07/17 13:22:25 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/07/20 14:53:29 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,40 @@ static int			ft_read(int fd, char **stack)
 	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[ret] = '\0';
-		temp = *stack;
-		*stack = ft_strjoin(*stack, buff);
-		free(temp);
+		if (!*stack)
+			*stack = ft_strdup(buff);
+		else
+		{
+			temp = *stack;
+			*stack = ft_strjoin(*stack, buff);
+			free(temp);
+		}
 		if (*stack && ft_strchr(*stack, '\n'))
 			break ;
 	}
 	return (ret);
 }
 
+static char			*ft_line(char *stack, char **line)
+{
+	int		i;
+	char	*temp;
+
+	i = 0;
+	while (stack[i] != '\n' && stack[i])
+		i++;
+	*line = ft_strsub(stack, 0, i);
+	temp = stack;
+	stack = ft_strdup(stack + i + 1);
+	free(temp);
+	return (stack);
+}
+
 int					get_next_line(const int fd, char **line)
 {
 	static char	*stack = NULL;
-	char		*temp;
-	int			i;
 	int			ret;
 
-	i = 0;
 	ret = 2;
 	stack == NULL ? stack = ft_strnew(0) : stack;
 	if (!(stack) || !line || fd < 0 || BUFF_SIZE <= 0)
@@ -51,12 +68,7 @@ int					get_next_line(const int fd, char **line)
 		return (0);
 	else
 	{
-		while (stack[i] != '\n' && stack[i])
-			i++;
-		*line = ft_strsub(stack, 0, i);
-		temp = stack;
-		stack = ft_strdup(stack + i + 1);
-		free(temp);
+		stack = ft_line(stack, line);
 	}
 	return (1);
 }
